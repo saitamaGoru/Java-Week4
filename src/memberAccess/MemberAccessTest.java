366f4200f9d56fd311be15811cc0a7772757bda5
pgrewa26 <300961555@cencol.ca>
2022-09-29 09:44:18 -0400

    Date class and employee package created

---
+++ src/memberAccess/MemberAccessTest.java
@@ -0,0 +1,16 @@
+package memberAccess;
+
+public class MemberAccessTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Time1 time = new Time1();
+		
+		time.hour = 12;
+		time.minute = 45;
+		time.second = 5;
+		
+		System.out.print(time);
+	}
+
+}
