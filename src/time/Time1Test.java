867687e534d687daa6bf613b6cec5fb37cc391cf
pgrewa26 <300961555@cencol.ca>
2022-09-29 09:06:09 -0400

    time class created

--- src/time/Time1Test.java
+++ src/time/Time1Test.java
@@ -4,7 +4,29 @@ public class Time1Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Time1 time = new Time1();
+		
+		displayTime("After time object is created",time);
+		System.out.println();
+		
+		time.setTime(13,27,6);
+		displayTime("After time object is created",time);
+		System.out.println();
+		
+		try
+		{
+			time.setTime(99, 99, 99);
+			
+		}
+		catch(IllegalArgumentException e)
+		{
+			System.out.printf("Exception:%s%n%n",e.getMessage());
+		}
+		displayTime("After calling setTime with invalid values",time);
 	}
-
+	
+	private static void displayTime(String header, Time1 t)
+	{
+		System.out.printf("%s%nUniversal Time: %s%nStandard Time: %s%n",header, t.ToUniversalString(),t.toString());
+	}
 }
